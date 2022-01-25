cee5e5af7d576222c0f9eadd48784f7b584308c5
Jesus Agamez <jagamez@celerik.com>
2022-01-25 18:18:53 -0500

    feat: chatt server with socket

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+
+
+const socketio = require('socket.io');
+const io = socketio(server, {
+    cors: {
+        origin: "http://localhost:3001",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on('connection', socket => {
+    let userName;
+
+    socket.on('connected', (name) => {
+        userName = name;
+        socket.broadcast.emit('messages', { name: 'Server', message: `${name} Ha entrado en la sala` });
+    });
+
+    socket.on('message', (name, message) => {
+        io.emit('messages', { name, message });
+    });
+
+    socket.on('disconnect', () => {
+        io.emit('messages', { name: 'Server', message: `${userName} Ha abandonado la sala` });
+    });
+});
+
+server.listen(3000, () => {
+    console.log('listening on port 3000');
+})
